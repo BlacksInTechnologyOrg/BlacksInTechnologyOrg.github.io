f97621dac710a14c8519b77c74c075116ecdddde
Herman Haggerty <dubh3124@gmail.com>
2018-08-11 12:24:49 -0700

    Created navbar with link to other BIT repo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,36 @@
-function Tabletop() {
-    Tabletop.init( { key: public_spreadsheet_url,
-                     callback: showInfo,
-                     parseNumbers: true } );
-  });
+var infosec_url = 'https://docs.google.com/spreadsheets/d/1aSsDMcspgwgaVdotnkne4DorQtTQHDYFTrpgboUMgeg/edit?usp=sharing';
+
+function infosec(){
+    Tabletop.init( { key: infosec_url,
+                    callback: infosecPull,
+                    parseNumbers: true } );
+                    
+};
     
-  function showInfo(data, tabletop) {
-    var source   = $("#cat-template").html();
+function infosecPull(data, tabletop) {
+    var source   = $("#infosec-resources").html();
     var template = Handlebars.compile(source);
-    $.each( tabletop.sheets("Cats").all(), function(i, cat) {
-      var html = template(cat);
-      $("#content").append(html);
+    $.each( tabletop.sheets("informationSecurity").all(), function(i, data) {
+    var html = template(data);
+    $("#content").append(html);
     });
-  }
+}
+
+function entrepreneur() {
+    Tabletop.init( { key: infosec_url,
+                    callback: entrepreneurPull,
+                    parseNumbers: true } );
+                    
+};
+    
+function entrepreneurPull(data, tabletop) {
+    var source   = $("#entrepreneur-resources").html();
+    var template = Handlebars.compile(source);
+    $.each( tabletop.sheets("entrepreneur").all(), function(i, data) {
+    var html = template(data);
+    $("#content").append(html);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', infosec)
+window.addEventListener('DOMContentLoaded', entrepreneur)
